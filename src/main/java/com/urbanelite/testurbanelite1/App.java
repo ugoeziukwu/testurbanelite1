db9a5b29d4385d8ad6c65207032f3a1664c9fc25
Eziukwu <ugochinyelu.eziukwu@capgemini.com>
2022-04-08 15:01:36 -0500

    added initial commit

---
+++ src/main/java/com/urbanelite/testurbanelite1/App.java
@@ -0,0 +1,13 @@
+package com.urbanelite.testurbanelite1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
